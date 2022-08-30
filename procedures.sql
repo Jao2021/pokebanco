046592392c86f6cb51c3ed193292f518a808389b
Jao2021 <alvesjp1920@outlook.com>
2022-08-30 11:05:30 -0300

    Add files via upload

--- procedures.sql
+++ procedures.sql
@@ -5,8 +5,8 @@ LANGUAGE plpgsql
 AS $$
 BEGIN
 	INSERT INTO Pokemon (ID, NOME, ALTURA, PESO) VALUES (V_ID, V_NOME, V_ALTURA, V_PESO);
-	INSERT INTO INFO (POKE_ID, REG, TIPO, JOGO) VALUES ((SELECT currval(pg_get_serial_sequence('Pokemon', 'id'))), V_REG, V_TIPO, V_JOGO);
-	INSERT INTO STATUS (POKE_ID, ATK, DEF, HP, SP_ATK, SP_DEF, SPEED) VALUES ((SELECT currval(pg_get_serial_sequence('Pokemon', 'id'))),
+	INSERT INTO INFO (POKE_ID, REG, TIPO, JOGO) VALUES (V_ID, V_REG, V_TIPO, V_JOGO);
+	INSERT INTO STATUS (POKE_ID, ATK, DEF, HP, SP_ATK, SP_DEF, SPEED) VALUES (V_ID,
 																			 V_ATK, V_DEF, V_HP, V_SP_ATK, V_SP_DEF, V_SPEED);
 	COMMIT;
 END;$$
@@ -14,4 +14,4 @@ END;$$
 CALL add_poke (001, 'Bulbassauro', 'kanto', 'Grama', 'Vermelho', 0.7, 6.9, 49, 49, 45, 65, 65, 45);
 INSERT INTO Pokemon (ID, NOME, ALTURA, PESO) VALUES (001, 'Bulbassauro', 0.7, 6.9);
 
-SELECT * FROM Pokemon;
+SELECT * FROM Pokemon, INFO, STATUS;
